e1d5fe7b06736c78a41cac911fd16779484d2682
Andrea Bardi <andbardii@icloud.com>
2025-03-20 15:24:13 +0100

    add

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 // Configurazione del database
-$db_host = 'localhost';
+$db_host = '34.154.158.134';
 $db_name = 'image_catalog';
-$db_user = 'root';
-$db_pass = 'veryverystrongpassword';
+$db_user = 'appmod-phpapp-user';
+$db_pass = '}`P{:{vE\†QX0%bT';
 
 try {
     $pdo = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);
